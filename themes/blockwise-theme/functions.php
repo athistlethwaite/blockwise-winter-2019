e1b9e71c16b849640766ce7d825a88b42bc42536
Gavin Jin <gavinjin62@gmail.com>
2019-03-13 10:45:18 -0700

    Enqueue fontawesome

--- themes/blockwise-theme/functions.php
+++ themes/blockwise-theme/functions.php
@@ -7,92 +7,99 @@
  * @package Blockwise_Theme
  */
 
-if ( ! function_exists( 'blockwise_setup' ) ) :
-/**
+if (!function_exists('blockwise_setup')) :
+	/**
  * Sets up theme defaults and registers support for various WordPress features.
  */
-function blockwise_setup() {
-	// Add default posts and comments RSS feed links to head.
-	add_theme_support( 'automatic-feed-links' );
+	function blockwise_setup()
+	{
+		// Add default posts and comments RSS feed links to head.
+		add_theme_support('automatic-feed-links');
 
-	// Let WordPress manage the document title.
-	add_theme_support( 'title-tag' );
+		// Let WordPress manage the document title.
+		add_theme_support('title-tag');
 
-	// Enable support for Post Thumbnails on posts and pages.
-	add_theme_support( 'post-thumbnails' );
+		// Enable support for Post Thumbnails on posts and pages.
+		add_theme_support('post-thumbnails');
 
-	// This theme uses wp_nav_menu() in one location.
-	register_nav_menus( array(
-		'primary' => esc_html( 'Primary Menu' ),
-	) );
+		// This theme uses wp_nav_menu() in one location.
+		register_nav_menus(array(
+			'primary' => esc_html('Primary Menu'),
+		));
 
-	// Switch search form, comment form, and comments to output valid HTML5.
-	add_theme_support( 'html5', array(
-		'search-form',
-		'comment-form',
-		'comment-list',
-		'gallery',
-		'caption',
-	) );
-
-}
+		// Switch search form, comment form, and comments to output valid HTML5.
+		add_theme_support('html5', array(
+			'search-form',
+			'comment-form',
+			'comment-list',
+			'gallery',
+			'caption',
+		));
+	}
 endif; // blockwise_setup
-add_action( 'after_setup_theme', 'blockwise_setup' );
+add_action('after_setup_theme', 'blockwise_setup');
 
 /**
  * Set the content width in pixels, based on the theme's design and stylesheet.
  *
  * @global int $content_width
  */
-function blockwise_content_width() {
-	$GLOBALS['content_width'] = apply_filters( 'blockwise_content_width', 640 );
+function blockwise_content_width()
+{
+	$GLOBALS['content_width'] = apply_filters('blockwise_content_width', 640);
 }
-add_action( 'after_setup_theme', 'blockwise_content_width', 0 );
+add_action('after_setup_theme', 'blockwise_content_width', 0);
 
 /**
  * Register widget area.
  *
  * @link https://developer.wordpress.org/themes/functionality/sidebars/#registering-a-sidebar
  */
-function blockwise_widgets_init() {
-	register_sidebar( array(
-		'name'          => esc_html( 'Sidebar' ),
+function blockwise_widgets_init()
+{
+	register_sidebar(array(
+		'name'          => esc_html('Sidebar'),
 		'id'            => 'sidebar-1',
 		'description'   => '',
 		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
 		'after_widget'  => '</aside>',
 		'before_title'  => '<h2 class="widget-title">',
 		'after_title'   => '</h2>',
-	) );
+	));
 }
-add_action( 'widgets_init', 'blockwise_widgets_init' );
+add_action('widgets_init', 'blockwise_widgets_init');
 
 /**
  * Filter the stylesheet_uri to output the minified CSS file.
  */
-function blockwise_minified_css( $stylesheet_uri, $stylesheet_dir_uri ) {
-	if ( file_exists( get_template_directory() . '/build/css/style.min.css' ) ) {
+function blockwise_minified_css($stylesheet_uri, $stylesheet_dir_uri)
+{
+	if (file_exists(get_template_directory() . '/build/css/style.min.css')) {
 		$stylesheet_uri = $stylesheet_dir_uri . '/build/css/style.min.css';
 	}
 
 	return $stylesheet_uri;
 }
-add_filter( 'stylesheet_uri', 'blockwise_minified_css', 10, 2 );
+add_filter('stylesheet_uri', 'blockwise_minified_css', 10, 2);
 
 /**
  * Enqueue scripts and styles.
  */
-function blockwise_scripts() {
-	wp_enqueue_style( 'red-starter-style', get_stylesheet_uri() );
+function blockwise_scripts()
+{
 
-	wp_enqueue_script( 'red-starter-navigation', get_template_directory_uri() . '/build/js/navigation.min.js', array(), '20151215', true );
-	wp_enqueue_script( 'red-starter-skip-link-focus-fix', get_template_directory_uri() . '/build/js/skip-link-focus-fix.min.js', array(), '20151215', true );
+	wp_enqueue_style('font-awesome-cdn', 'https://use.fontawesome.com/releases/v5.7.2/css/all.css', array(), '5.7.2');
 
-	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
-		wp_enqueue_script( 'comment-reply' );
+	wp_enqueue_style('red-starter-style', get_stylesheet_uri());
+
+	wp_enqueue_script('red-starter-navigation', get_template_directory_uri() . '/build/js/navigation.min.js', array(), '20151215', true);
+	wp_enqueue_script('red-starter-skip-link-focus-fix', get_template_directory_uri() . '/build/js/skip-link-focus-fix.min.js', array(), '20151215', true);
+
+	if (is_singular() && comments_open() && get_option('thread_comments')) {
+		wp_enqueue_script('comment-reply');
 	}
 }
-add_action( 'wp_enqueue_scripts', 'blockwise_scripts' );
+add_action('wp_enqueue_scripts', 'blockwise_scripts');
 
 /**
  * Custom template tags for this theme.
