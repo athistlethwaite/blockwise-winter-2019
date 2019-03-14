4d61fdbcd19902eba6220deb4a84347905f047df
Gavin Jin <gavinjin62@gmail.com>
2019-03-14 15:17:02 -0700

    structure for membership list

--- themes/blockwise-theme/membership.php
+++ themes/blockwise-theme/membership.php
@@ -9,30 +9,44 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+<div id="primary" class="content-area">
+    <main id="main" class="site-main" role="main">
 
-		<?php if ( have_posts() ) : ?>
+        <?php if (have_posts()) : ?>
 
-			<?php if ( is_home() && ! is_front_page() ) : ?>
-				<header>
-					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
-				</header>
-			<?php endif; ?>
+        <?php if (is_home() && !is_front_page()) : ?>
+        <header>
+            <h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
+        </header>
+        <?php endif; ?>
 
-			<?php /* Start the Loop */ ?>
-			<?php while ( have_posts() ) : the_post(); ?>
+        <?php  /* Start the Loop */ ?>
+        <?php while (have_posts()) : the_post(); ?>
 
-				<?php get_template_part( 'template-parts/content' ); ?>
+        <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+            <header class="entry-header">
+                <?php the_title(sprintf('<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url(get_permalink())), '</a></h2>'); ?>
+            </header><!-- .entry-header -->
 
-			<?php endwhile; ?>
+            <div class="entry-content">
+                <?php the_content(); ?>
+                <?php
+				wp_link_pages(array(
+					'before' => '<div class="page-links">' . esc_html('Pages:'),
+					'after'  => '</div>',
+				));
+				?>
+            </div><!-- .entry-content -->
+        </article><!-- #post-## -->
 
-		<?php else : ?>
+        <?php endwhile; ?>
 
-			<?php get_template_part( 'template-parts/content', 'none' ); ?>
+        <?php else : ?>
 
-		<?php endif; ?>
+        <?php get_template_part('template-parts/content', 'none'); ?>
 
-		</main><!-- #main -->
-	</div><!-- #primary -->
-<?php get_footer(); ?>
+        <?php endif; ?>
+
+    </main><!-- #main -->
+</div><!-- #primary -->
+<?php get_footer(); ?> 
