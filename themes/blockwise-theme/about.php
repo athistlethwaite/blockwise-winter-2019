ccd7bc9835496685f04397f0021ce91ce09788d5
MarieFocke <mariefocke@gmail.com>
2019-03-14 11:12:30 -0700

    gutenburg of the about.php

--- themes/blockwise-theme/about.php
+++ themes/blockwise-theme/about.php
@@ -26,8 +26,14 @@ get_header(); ?>
         <header class="entry-header">
             <?php the_title(sprintf('<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url(get_permalink())), '</a></h2>'); ?>
             <div class="entry-content">
-                <?php the_excerpt(); ?>
-            </div><!-- .entry-content -->
+                <?php the_content(); ?>
+                <?php
+                    wp_link_pages( array(
+                        'before' => '<div class="page-links">' . esc_html( 'Pages:' ),
+                        'after'  => '</div>',
+                    ) );
+                ?>
+            </div>
         </header>
         <!--.entry-header-->
 
