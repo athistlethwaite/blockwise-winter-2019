d4c62c966cbb8cd03a2026c7677e17603d911ecd
MarieFocke <mariefocke@gmail.com>
2019-03-14 15:47:01 -0700

    content page about

--- themes/blockwise-theme/about.php
+++ themes/blockwise-theme/about.php
@@ -38,37 +38,56 @@ get_header(); ?>
         <!--.entry-header-->
 
 				<?php endwhile; ?>
-
-        <section>
-					<h2>Our Partners</h2>
-					<div class="corporate-member">	
-						<p>Corporate Members</p>
-                <ul>
-                    <?php $loop = new WP_Query(array('post_type' => 'partner')); ?>
-                    <?php while ($loop->have_posts()) : $loop->the_post(); ?>
-											<?php get_template_part('template-parts/content'); ?>
-                    <?php endwhile; wp_reset_query(); ?>
-                </ul>
-						<div class="become-member">
-							<a class="full-colored" href="">Become A Member</a>
-						</div>
-          </div>
-				</section>
+                <section>
+				    <div class="corporate-member">	
+					<p>Corporate Members</p>
+                        <ul>
+                            <?php $loop = new WP_Query(array('post_type' => 'partner')); ?>
+                            <?php while ($loop->have_posts()) : $loop->the_post(); ?>
+                                <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+                                    <header class="entry-header">
+                                        <?php if ( has_post_thumbnail() ) : ?>
+                                            <?php the_post_thumbnail( 'large' ); ?>
+                                        <?php endif; ?>
+                                    </header><!-- .entry-header -->
+                                    <div class="entry-content">
+                                        <?php the_excerpt(); ?>
+                                        <div class="member-website">
+                                            <a class="border-colored" href="">Visit Website</a>
+                                        </div>
+                                        <div class="become-member">
+                                            <a class="full-colored" href="<?php echo get_page_link(48); ?>">Become A Member</a>
+                                        </div>
+                                    </div><!-- .entry-content -->
+                                </article><!-- #post-## -->
+                            <?php endwhile; wp_reset_query(); ?> 
+                        </ul>
+                    </div>
+                </section>
 
 				<section>
-						<div class="corporate-mentor">	
-						<p>Community Mentors</p>
-                <ul>
-                    <?php $loop = new WP_Query(array('post_type' => 'mentor')); ?>
-                    <?php while ($loop->have_posts()) : $loop->the_post(); ?>
-											<?php get_template_part('template-parts/content'); ?>
-                    <?php endwhile; wp_reset_query(); ?> 
-                </ul>
-            </div>
-						<div class="become-mentor">
-							<a class="full-colored" href="">Become A Mentor</a>
-						</div>
-				</section>
+				    <div class="corporate-mentor">	
+					<p>Community Mentors</p>
+                        <ul>
+                            <?php $loop = new WP_Query(array('post_type' => 'mentor')); ?>
+                            <?php while ($loop->have_posts()) : $loop->the_post(); ?>
+                                <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+                                    <header class="entry-header">
+                                        <?php if ( has_post_thumbnail() ) : ?>
+                                            <?php the_post_thumbnail( 'large' ); ?>
+                                        <?php endif; ?>
+                                    </header><!-- .entry-header -->
+                                    <div class="entry-content">
+                                        <?php the_excerpt(); ?>
+                                        <div class="become-mentor">
+                                            <a class="full-colored" href="<?php echo get_page_link(48); ?>">Become A Mentor</a>
+                                        </div>
+                                    </div><!-- .entry-content -->
+                                </article><!-- #post-## -->
+                            <?php endwhile; wp_reset_query(); ?> 
+                        </ul>
+                    </div>
+                </section>
 
         <?php else : ?>
 
--- themes/blockwise-theme/template-parts/content.php
+++ themes/blockwise-theme/template-parts/content.php
@@ -12,8 +12,6 @@
 		<?php if ( has_post_thumbnail() ) : ?>
 			<?php the_post_thumbnail( 'large' ); ?>
 		<?php endif; ?>
-
-		<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
 	</header><!-- .entry-header -->
 
 	<div class="entry-content">
