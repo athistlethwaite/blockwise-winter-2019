1d8fa8a8917af816fc2e3435832d23d862b0d471
MarieFocke <mariefocke@gmail.com>
2019-03-13 22:08:23 -0700

    content init

--- themes/blockwise-theme/archive-event.php
+++ themes/blockwise-theme/archive-event.php
@@ -13,24 +13,24 @@ get_header(); ?>
 		<?php if ( have_posts() ) : ?>
 
 			<header class="page-header">
-				<?php
-					the_archive_title( '<h1 class="page-title">', '</h1>' );
-					the_archive_description( '<div class="taxonomy-description">', '</div>' );
-				?>
+				<?php the_archive_title( '<h1 class="page-title">', '</h1>' );?>
+				<h3 class="event-subtitle">Connecting BC’s Blockchain community one event at a time.</h3>
 			</header><!-- .page-header -->
-
 			<?php /* Start the Loop */ ?>
 			<?php while ( have_posts() ) : the_post(); ?>
-
-				<?php
-					get_template_part( 'template-parts/content' );
-				?>
-
+				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+					<header class="entry-header">
+						<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
+						<p class="event-date"><?php echo CFS()->get('event_date'); ?></p>
+						<p class="event-location"><?php echo CFS()->get('event_location'); ?></p>
+						<p class="event-host"><?php echo CFS()->get('event_host'); ?></p>
+					</header>
+				</article>
 			<?php endwhile; ?>
 
 		<?php else : ?>
 
-			<?php get_template_part( 'template-parts/content', 'none' ); ?>
+			<?php get_template_part( 'template-parts/content'); ?>
 
 		<?php endif; ?>
 
