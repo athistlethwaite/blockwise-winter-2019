4e5783d1323d142dedb1c6c6a5b1850c7539626f
MarieFocke <mariefocke@gmail.com>
2019-03-15 11:21:31 -0700

    inputs contact add

--- themes/blockwise-theme/template-parts/content-contact.php
+++ themes/blockwise-theme/template-parts/content-contact.php
@@ -11,19 +11,12 @@
 	<header class="entry-header">
 		<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
 	</header><!-- .entry-header -->
-
-	<div class="entry-content">
-        <form clas="container">
-                    <input id="name" type="text" name="name">
-                    <br>
-                    <input id="company" type="text" name="compagy">
-                    <br>
-                    <input id="email" type="text" name="email">
-                    <br>
-                    <input id="phone-number" type="text" name="phone-number">
-                    <br>
-                    <textarea id="question" type="text">Enter a question</textarea>
-                    <button id="submit" type="submit" name="submit" class="full-colored">Submit</button>
-        </form>
-	</div><!-- .entry-content -->
+        <div class="entry-content">
+                <?php the_content(); ?>
+                <?php
+		        wp_link_pages(array(
+			'before' => '<div class="page-links">' . esc_html('Pages:'),
+			'after'  => '</div>',
+			));
+		?>
 </article><!-- #post-## -->
