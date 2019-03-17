882a7db5e541c10c981e9aaf8000ed530530cf26
Emre Kaynak <emrekaynak94@gmail.com>
2019-03-16 17:51:56 -0700

    Add containers

--- themes/blockwise-theme/footer.php
+++ themes/blockwise-theme/footer.php
@@ -9,17 +9,19 @@
 </div><!-- #content -->
 
 <footer id="colophon" class="site-footer" role="contentinfo">
-    <div class="site-info">
-        <div class="social-icons">
-            <a href="#"><i class="fab fa-facebook-square"></i></a>
-            <a href="#"><i class="fab fa-instagram"></i></a>
-            <a href="#"><i class="fab fa-linkedin"></i></a>
-            <a href="#"><i class="fab fa-twitter-square"></i></a>
+    <div class="container">
+        <div class="site-info">
+            <div class="social-icons">
+                <a href="#"><i class="fab fa-facebook-square"></i></a>
+                <a href="#"><i class="fab fa-instagram"></i></a>
+                <a href="#"><i class="fab fa-linkedin"></i></a>
+                <a href="#"><i class="fab fa-twitter-square"></i></a>
+            </div>
+            <span class="copyright">&copy; 2019 Blockwise. All Rights Reserved.</span>
+        </div><!-- .site-info -->
+        <div class="footer-menu">
+            <?php wp_nav_menu(array('theme_location' => 'secondary')); ?>
         </div>
-        <span class="copyright">&copy; 2019 Blockwise. All Rights Reserved.</span>
-    </div><!-- .site-info -->
-    <div class="footer-menu">
-        <?php wp_nav_menu(array('theme_location' => 'secondary')); ?>
     </div>
 </footer><!-- #colophon -->
 </div><!-- #page -->
--- themes/blockwise-theme/header.php
+++ themes/blockwise-theme/header.php
@@ -22,17 +22,19 @@
     <div id="page" class="hfeed site">
         <a class="skip-link screen-reader-text" href="#content"><?php echo esc_html('Skip to content'); ?></a>
         <header id="masthead" class="site-header" role="banner">
-            <div class="site-branding">
-                <a href="<?php echo esc_url(home_url('/')); ?>" rel="home">
-                    <img alt="Blockwise logo" src="<?php echo get_template_directory_uri(); ?>/assets/images/BlockwiseLogo.png">
-                </a>
-                <h1 class="site-title"><a href="<?php echo esc_url(home_url('/')); ?>" rel="home"><?php bloginfo('name'); ?></a></h1>
-            </div><!-- .site-branding -->
+            <div class="container">
+                <div class="site-branding">
+                    <a href="<?php echo esc_url(home_url('/')); ?>" rel="home">
+                        <img alt="Blockwise logo" src="<?php echo get_template_directory_uri(); ?>/assets/images/BlockwiseLogo.png">
+                    </a>
+                    <h1 class="site-title"><a href="<?php echo esc_url(home_url('/')); ?>" rel="home"><?php bloginfo('name'); ?></a></h1>
+                </div><!-- .site-branding -->
 
-            <nav id="site-navigation" class="main-navigation" role="navigation">
-                <button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php echo esc_html('Primary Menu'); ?></button>
-                <?php wp_nav_menu(array('theme_location' => 'primary', 'menu_id' => 'primary-menu')); ?>
-            </nav><!-- #site-navigation -->
+                <nav id="site-navigation" class="main-navigation" role="navigation">
+                    <button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php echo esc_html('Primary Menu'); ?></button>
+                    <?php wp_nav_menu(array('theme_location' => 'primary', 'menu_id' => 'primary-menu')); ?>
+                </nav><!-- #site-navigation -->
+            </div>
         </header><!-- #masthead -->
 
         <div id="content" class="site-content container"> 
