43c898d585475b0f455f43d504e14f9b251d14a3
Emre Kaynak <emrekaynak94@gmail.com>
2019-03-18 14:28:03 -0700

    Refactor for frontpage events template

--- themes/blockwise-theme/template-parts/content-event.php
+++ themes/blockwise-theme/template-parts/content-event.php
@@ -11,12 +11,10 @@
     <header class="entry-header">
         <a href="#">
             <h2 class="entry-title"><?php the_title(); ?></h2>
-            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/MembershipDropArrow.png" alt="Click for Event Info">
         </a>
         <div class="entry-meta">
             <p class="event-date"><?php echo CFS()->get('event_date'); ?></p>
             <p class="event-location"><?php echo CFS()->get('event_location'); ?></p>
         </div><!-- .entry-meta -->
     </header><!-- .entry-header -->
-    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/LearnChainLink.png" alt="Blockwise chains">
 </article><!-- #post-## --> 
