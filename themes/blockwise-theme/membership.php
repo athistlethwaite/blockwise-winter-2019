2441ac066c649d8921add58765f832f96b0cf77c
Gavin Jin <gavinjin62@gmail.com>
2019-03-19 13:53:51 -0700

    membership content ordering

--- themes/blockwise-theme/membership.php
+++ themes/blockwise-theme/membership.php
@@ -15,30 +15,7 @@ get_header(); ?>
         <?php while (have_posts()) : the_post(); ?>
 
         <?php get_template_part('template-parts/content', 'membership'); ?>
-        <section class="option-wrapper">
-            <?php
-    /*
-     * CFS Loop membership_options Title
-     */
-            $fields = CFS()->get('membership_options');
-            foreach ($fields as $field) : ?>
-
-            <?php echo "<div class='option-title-list'><h4>" . $field['option_title'] . "</h4>"; ?>
-
-            <ul>
-                <?php 
-                /*
-         * CFS Loop list options
-         */
-                $options = $field['options_list_items'];
-                foreach ($options as $option) {
-
-                    echo "<li>" . $option['option'] . "</li>";
-                } ?>
-
-            </ul>
-</div>
-<?php endforeach; ?>
+       
 <?php endwhile; ?>
 
 </section>
--- themes/blockwise-theme/template-parts/content-membership.php
+++ themes/blockwise-theme/template-parts/content-membership.php
@@ -16,12 +16,36 @@
     </header><!-- .entry-header -->
 
     <div class="entry-content">
+        <section class="option-wrapper">
+            <?php
+    /*
+     * CFS Loop membership_options Title
+     */
+            $fields = CFS()->get('membership_options');
+            foreach ($fields as $field) : ?>
+
+            <?php echo "<div class='option-title-list'><h4>" . $field['option_title'] . "</h4>"; ?>
+
+            <ul>
+                <?php 
+                /*
+         * CFS Loop list options
+         */
+                $options = $field['options_list_items'];
+                foreach ($options as $option) {
+
+                    echo "<li>" . $option['option'] . "</li>";
+                } ?>
+
+            </ul>
+    </div>
+    <?php endforeach; ?>
         <?php the_content(); ?>
-        <?php
-        wp_link_pages(array(
-            'before' => '<div class="page-links">' . esc_html('Pages:'),
-            'after'  => '</div>',
-        ));
-        ?>
+    <?php
+    wp_link_pages(array(
+        'before' => '<div class="page-links">' . esc_html('Pages:'),
+        'after'  => '</div>',
+    ));
+    ?>
     </div><!-- .entry-content -->
 </article><!-- #post-## --> 
