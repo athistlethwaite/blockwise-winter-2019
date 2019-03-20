50587c9f63ccdd95b8a0df356d912e638e0a49a3
Alysha Thistlethwaite <alysha.thistlethwaite@gmail.com>
2019-03-20 15:46:18 -0700

    Toggle menu for lessons.

--- themes/blockwise-theme/js/toggle.js
+++ themes/blockwise-theme/js/toggle.js
@@ -1,11 +1,28 @@
 (function ($) {
-  $(".lesson-type-title").on("click", function () {
-    $(this).next().toggle();
+  $("#button1").on("click", function () {
+    console.log($(this).parent().parent());
+    $(this).parent().parent().find('ul').toggle();
+    $("#button2").parent().parent().find('ul').fadeOut();
+    $("#button3").parent().parent().find('ul').fadeOut();
+  });
+
+  $("#button2").on("click", function () {
+    console.log($(this).parent().parent());
+    $(this).parent().parent().find('ul').toggle();
+    $("#button1").parent().parent().find('ul').fadeOut();
+    $("#button3").parent().parent().find('ul').fadeOut();
+  });
+
+  $("#button3").on("click", function () {
+    console.log($(this).parent().parent());
+    $(this).parent().parent().find('ul').toggle();
+    $("#button1").parent().parent().find('ul').fadeOut();
+    $("#button2").parent().parent().find('ul').fadeOut();
   });
 
   $(".back-to-lesson").on("click", function () {
     event.preventDefault();
-    $('#load-content').html('');
+    $('#load-content').html("");
   });
 
 })(jQuery);
