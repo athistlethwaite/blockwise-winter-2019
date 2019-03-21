65ddab6c62560e64376b10d55041893208e5197c
Gavin Jin <gavinjin62@gmail.com>
2019-03-20 23:34:04 -0700

    add tab menu color toggle

--- themes/blockwise-theme/js/membership-script.js
+++ themes/blockwise-theme/js/membership-script.js
@@ -3,28 +3,72 @@
  */
 
 jQuery(function($) {
-  $(window).resize(function() {
-    if ($(window).width() < 600) {
+  if ($(window).width() < 600) {
+    $('.tablinks li:first-child').addClass('tab-color');
+    $('.option-wrapper ul:first-child').addClass('active');
+    $('.tablinks li:first-child').click(function() {
+      $('.tablinks li:first-child').addClass('tab-color');
+      $('.tablinks li:nth-child(2n)').removeClass('tab-color');
+      $('.tablinks li:nth-child(3n)').removeClass('tab-color');
       $('.option-wrapper ul:first-child').addClass('active');
-      $('.tablinks li:first-child').click(function() {
-        $('.option-wrapper ul:first-child').addClass('active');
-        $('.option-wrapper ul:nth-child(2n)').removeClass('active');
-        $('.option-wrapper ul:nth-child(3n)').removeClass('active');
-      });
-      $('.tablinks li:nth-child(2n)').click(function() {
-        $('.option-wrapper ul:nth-child(2n)').addClass('active');
-        $('.option-wrapper ul:first-child').removeClass('active');
-        $('.option-wrapper ul:nth-child(3n)').removeClass('active');
-      });
-      $('.tablinks li:nth-child(3n)').click(function() {
-        $('.option-wrapper ul:nth-child(3n)').addClass('active');
-        $('.option-wrapper ul:first-child').removeClass('active');
-        $('.option-wrapper ul:nth-child(2n)').removeClass('active');
-      });
-    } else {
-      $('.option-wrapper ul:first-child').addClass('active');
+      $('.option-wrapper ul:nth-child(2n)').removeClass('active');
+      $('.option-wrapper ul:nth-child(3n)').removeClass('active');
+    });
+    $('.tablinks li:nth-child(2n)').click(function() {
+      $('.tablinks li:nth-child(2n)').addClass('tab-color');
+      $('.tablinks li:first-child').removeClass('tab-color');
+      $('.tablinks li:nth-child(3n)').removeClass('tab-color');
       $('.option-wrapper ul:nth-child(2n)').addClass('active');
+      $('.option-wrapper ul:first-child').removeClass('active');
+      $('.option-wrapper ul:nth-child(3n)').removeClass('active');
+    });
+    $('.tablinks li:nth-child(3n)').click(function() {
+      $('.tablinks li:nth-child(3n)').addClass('tab-color');
+      $('.tablinks li:first-child').removeClass('tab-color');
+      $('.tablinks li:nth-child(2n)').removeClass('tab-color');
       $('.option-wrapper ul:nth-child(3n)').addClass('active');
-    }
-  });
+      $('.option-wrapper ul:first-child').removeClass('active');
+      $('.option-wrapper ul:nth-child(2n)').removeClass('active');
+    });
+  } else {
+    $('.option-wrapper ul:first-child').addClass('active');
+    $('.option-wrapper ul:nth-child(2n)').addClass('active');
+    $('.option-wrapper ul:nth-child(3n)').addClass('active');
+  }
+
+  //   let resizeEvent = 0;
+
+  //   if ($(window).width() < 600) {
+  //     console.log('mobile size');
+  //   } else {
+  //     console.log('>mobile size');
+  //     resizeEvent = 1;
+  //   }
+
+  //   $(window).on('resize', function() {
+  //     if (!resizeEvent) {
+  //       if ($(window).width() < 600) {
+  //         $('.option-wrapper ul:first-child').addClass('active');
+  //         $('.tablinks li:first-child').click(function() {
+  //           $('.option-wrapper ul:first-child').addClass('active');
+  //           $('.option-wrapper ul:nth-child(2n)').removeClass('active');
+  //           $('.option-wrapper ul:nth-child(3n)').removeClass('active');
+  //         });
+  //         $('.tablinks li:nth-child(2n)').click(function() {
+  //           $('.option-wrapper ul:nth-child(2n)').addClass('active');
+  //           $('.option-wrapper ul:first-child').removeClass('active');
+  //           $('.option-wrapper ul:nth-child(3n)').removeClass('active');
+  //         });
+  //         $('.tablinks li:nth-child(3n)').click(function() {
+  //           $('.option-wrapper ul:nth-child(3n)').addClass('active');
+  //           $('.option-wrapper ul:first-child').removeClass('active');
+  //           $('.option-wrapper ul:nth-child(2n)').removeClass('active');
+  //         });
+  //       } else {
+  //         $('.option-wrapper ul:first-child').addClass('active');
+  //         $('.option-wrapper ul:nth-child(2n)').addClass('active');
+  //         $('.option-wrapper ul:nth-child(3n)').addClass('active');
+  //       }
+  //     }
+  //   });
 });
