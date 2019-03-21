010e88821955bf3134f87acc3ff2606564858aee
Gavin Jin <gavinjin62@gmail.com>
2019-03-20 22:25:38 -0700

    add resize event for tab menu

--- themes/blockwise-theme/js/membership-script.js
+++ themes/blockwise-theme/js/membership-script.js
@@ -3,22 +3,28 @@
  */
 
 jQuery(function($) {
-  $('.option-wrapper ul:first-child').addClass('active');
-  console.log($('.option-wrapper ul:first-child'));
-  console.log($('.tablinks ul:first-child'));
-  $('.tablinks li:first-child').click(function() {
-    $('.option-wrapper ul:first-child').addClass('active');
-    $('.option-wrapper ul:nth-child(2n)').removeClass('active');
-    $('.option-wrapper ul:nth-child(3n)').removeClass('active');
-  });
-  $('.tablinks li:nth-child(2n)').click(function() {
-    $('.option-wrapper ul:nth-child(2n)').addClass('active');
-    $('.option-wrapper ul:first-child').removeClass('active');
-    $('.option-wrapper ul:nth-child(3n)').removeClass('active');
-  });
-  $('.tablinks li:nth-child(3n)').click(function() {
-    $('.option-wrapper ul:nth-child(3n)').addClass('active');
-    $('.option-wrapper ul:first-child').removeClass('active');
-    $('.option-wrapper ul:nth-child(2n)').removeClass('active');
+  $(window).resize(function() {
+    if ($(window).width() < 600) {
+      $('.option-wrapper ul:first-child').addClass('active');
+      $('.tablinks li:first-child').click(function() {
+        $('.option-wrapper ul:first-child').addClass('active');
+        $('.option-wrapper ul:nth-child(2n)').removeClass('active');
+        $('.option-wrapper ul:nth-child(3n)').removeClass('active');
+      });
+      $('.tablinks li:nth-child(2n)').click(function() {
+        $('.option-wrapper ul:nth-child(2n)').addClass('active');
+        $('.option-wrapper ul:first-child').removeClass('active');
+        $('.option-wrapper ul:nth-child(3n)').removeClass('active');
+      });
+      $('.tablinks li:nth-child(3n)').click(function() {
+        $('.option-wrapper ul:nth-child(3n)').addClass('active');
+        $('.option-wrapper ul:first-child').removeClass('active');
+        $('.option-wrapper ul:nth-child(2n)').removeClass('active');
+      });
+    } else {
+      $('.option-wrapper ul:first-child').addClass('active');
+      $('.option-wrapper ul:nth-child(2n)').addClass('active');
+      $('.option-wrapper ul:nth-child(3n)').addClass('active');
+    }
   });
 });
