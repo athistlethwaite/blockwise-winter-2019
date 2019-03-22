eea54e8a7f22506536f9dcaacad27d19661dda8d
Gavin Jin <gavinjin62@gmail.com>
2019-03-22 13:25:58 -0700

    add close button to popup membership

--- themes/blockwise-theme/js/membership-script.js
+++ themes/blockwise-theme/js/membership-script.js
@@ -2,12 +2,12 @@
  *   Membership page tab menu for mobile
  */
 
-jQuery(function ($) {
+jQuery(function($) {
   if ($(window).width() < 600) {
     //wrap in new function call at beginning
     $('.tablinks li:first-child').addClass('tab-color');
     $('.option-wrapper ul:first-child').addClass('active');
-    $('.tablinks li:first-child').click(function () {
+    $('.tablinks li:first-child').click(function() {
       $('.tablinks li:first-child').addClass('tab-color');
       $('.tablinks li:nth-child(2n)').removeClass('tab-color');
       $('.tablinks li:nth-child(3n)').removeClass('tab-color');
@@ -15,7 +15,7 @@ jQuery(function ($) {
       $('.option-wrapper ul:nth-child(2n)').removeClass('active');
       $('.option-wrapper ul:nth-child(3n)').removeClass('active');
     });
-    $('.tablinks li:nth-child(2n)').click(function () {
+    $('.tablinks li:nth-child(2n)').click(function() {
       $('.tablinks li:nth-child(2n)').addClass('tab-color');
       $('.tablinks li:first-child').removeClass('tab-color');
       $('.tablinks li:nth-child(3n)').removeClass('tab-color');
@@ -23,7 +23,7 @@ jQuery(function ($) {
       $('.option-wrapper ul:first-child').removeClass('active');
       $('.option-wrapper ul:nth-child(3n)').removeClass('active');
     });
-    $('.tablinks li:nth-child(3n)').click(function () {
+    $('.tablinks li:nth-child(3n)').click(function() {
       $('.tablinks li:nth-child(3n)').addClass('tab-color');
       $('.tablinks li:first-child').removeClass('tab-color');
       $('.tablinks li:nth-child(2n)').removeClass('tab-color');
@@ -40,26 +40,23 @@ jQuery(function ($) {
   $('.sign-up-pop').click(function() {
     $('.wpcf7-form').addClass('mobile-popup');
   });
-  $('.membership').click(function () {
+  //on sign-up click close popup, show thankyou pop up
+  $('.wpcf7-submit').click(function() {
     $('.wpcf7-form').removeClass('mobile-popup');
     $('.thank-you').addClass('thank-you-active');
   });
-  $('.got-it').click(function () {
+  // click got-it remove thankyou popup
+  $('.got-it').click(function() {
     $('.thank-you').removeClass('thank-you-active');
   });
 
-  $('.mobile-popup').on('blur', function() {
-    $('.mobile-popup').removeClass('mobile-popup');
-  });
-
-  $('.thank-you').on('blur', function() {
-    $('.thank-you').removeClass('thank-you-active');
+  $('.close').click(function() {
+    event.preventDefault();
+    $('.wpcf7-form').removeClass('mobile-popup');
   });
 
   // click anything not in the popup div
-  $('.wpcf7-form').click('not', function() {
-    $('.wpcf7-form').removeClass('mobile-popup');
-  });
+
   // remove the pop up div class
 
   //   let resizeEvent = 0;
