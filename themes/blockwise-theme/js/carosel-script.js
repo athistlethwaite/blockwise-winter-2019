1770b9dd783f2c8155b6dc71b517e12439f945eb
Emre Kaynak <emrekaynak94@gmail.com>
2019-03-24 16:37:39 -0700

    Add carosel movement

--- themes/blockwise-theme/js/carosel-script.js
+++ themes/blockwise-theme/js/carosel-script.js
@@ -25,4 +25,25 @@
       '<a style="left:1rem" class="carosel-nav left">&#10094;</a><a style="right:1rem" class="carosel-nav right">&#10095;</a></div>';
     carosel.html(viewPort);
   }
+
+  $('.carosel').on('click', '.carosel-nav', function() {
+    const viewPort = $(this).parent();
+    const direction = $(this).attr('class');
+    if (direction.includes('right')) {
+      caroselSlideRight(viewPort);
+    } else if (direction.includes('left')) {
+      caroselSlideLeft(viewPort);
+    }
+  });
+
+  function caroselSlideRight(viewPort) {
+    const item = viewPort.children('.carosel-item:first-child');
+    item.remove();
+    viewPort.append(item);
+  }
+  function caroselSlideLeft(viewPort) {
+    const item = viewPort.children('.carosel-item:last-child');
+    item.remove();
+    viewPort.prepend(item);
+  }
 })(jQuery);
