0155bc6862916e5f619380a5a02e3a9eec481733
Alysha Thistlethwaite <alysha.thistlethwaite@gmail.com>
2019-03-26 11:53:40 -0700

    Create and work on 404 page

--- themes/blockwise-theme/404.php
+++ themes/blockwise-theme/404.php
@@ -15,11 +15,11 @@ get_header(); ?>
         <section class="error-404 not-found">
             <header class="page-header">
                 <h1 class="page-title"><?php echo esc_html('Oops! That page can&rsquo;t be found.'); ?></h1>
+
+                <p><?php echo esc_html('It looks like nothing was found at this location.'); ?></p>
+
             </header><!-- .page-header -->
 
-            <div class="page-content">
-                <p><?php echo esc_html('It looks like nothing was found at this location.'); ?></p>
-            </div><!-- .page-content -->
         </section><!-- .error-404 -->
 
     </main><!-- #main -->
