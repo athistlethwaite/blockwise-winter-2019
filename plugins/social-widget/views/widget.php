9530c0a64988af5f6bdea30994821d7d8cb7c59c
Alysha Thistlethwaite <alysha.thistlethwaite@gmail.com>
2019-03-26 10:43:34 -0700

    Fix widget

--- plugins/social-widget/views/widget.php
+++ plugins/social-widget/views/widget.php
@@ -7,18 +7,18 @@
 
 <?php if(strlen( trim( $instagram) ) > 0) : ?>
 <p>
-   <span class="social-icons"><a href="<?php echo $instagram; ?>"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/insta-icon.png" alt="Instagram Icon"></a></span> <?php echo $instagram; ?>
+   <span class="social-icons"><a href="<?php echo $instagram; ?>"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/insta-icon.png" alt="Instagram Icon"></a></span> 
 </p>
 <?php endif; ?>
 
 <?php if(strlen( trim( $linkedin) ) > 0) : ?>
 <p>
-   <span class="social-icons"><a href="<?php echo $linkedin; ?>"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/linkedin-icon.png" alt="LinkedIn Icon"></a></span> <?php echo $linkedin; ?>
+   <span class="social-icons"><a href="<?php echo $linkedin; ?>"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/linkedin-icon.png" alt="LinkedIn Icon"></a></span> 
 </p>
 <?php endif; ?>
 
 <?php if(strlen( trim( $twitter) ) > 0) : ?>
 <p>
-   <span class="social-icons"><a href="<?php echo $twitter; ?>"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/twitter-icon.png" alt="Twitter Icon"></a></span> <?php echo $twitter; ?>
+   <span class="social-icons"><a href="<?php echo $twitter; ?>"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/twitter-icon.png" alt="Twitter Icon"></a></span> 
 </p>
 <?php endif; ?>
